c55efea051b875a9b18f64666df19866a72c2140
aishwinder <aishwinder.sandhu@gmail.com>
2026-04-30 07:30:17 +0530

    Updated cors variables

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Image Processing API")
 origins = [
-    "http://localhost:5173" #no trailing / for adding any url
+    "http://localhost:5173", #no trailing / for adding any url
     "https://faceanalyzer-ui.netlify.app"
 ]
 app.add_middleware(
